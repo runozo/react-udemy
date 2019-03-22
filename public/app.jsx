538790565dcbc37398fe117b1bebf872626211c4
Cesare Ghirelli <cghirelli@arpae.it>
2019-03-22 14:51:15 +0100

    stage 1

--- public/app.jsx
+++ public/app.jsx
@@ -1,14 +1,48 @@
 var Greeter = React.createClass({
+    getDefaultProps: function() {
+        return {
+            name: 'React',
+            message: 'This is from a component'
+        }
+    },
+    getInitialState: function() {
+        return {
+            name: this.props.name
+        };
+    },
+    onButtonClick: function(e) {
+        e.preventDefault();
+
+        var nameRef = this.refs.name;
+        var name = nameRef.value;
+        nameRef.value = '';
+
+        if (typeof(name) === 'string' && name.length > 0) {
+            this.setState({
+                name: name
+            });
+        }
+    },
     render: function() {
+        var name = this.state.name;
+        var message = this.props.message;
+
         return (
             <div>
-                <h1>Hello React2!</h1>
-                <p>This is from a component</p>
+                <h1>Hello {name}!</h1>
+                <p>{message + '!!'}</p>
+                <form onSubmit={this.onButtonClick}>
+                    <input type="text" ref="name"/>
+                    <button>Set name</button>
+                </form>
             </div>
         );
     }
 });
+
+var firstName = 'Cesare';
+
 ReactDOM.render(
-    <Greeter/>,
+    <Greeter name={firstName} message="Message is from prop!"/>,
     document.getElementById('app')
 );
