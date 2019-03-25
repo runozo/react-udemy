e30941eeb5d09f2c20dce9fec3765fad35da29d7
Cesare Ghirelli <cghirelli@arpae.it>
2019-03-25 11:32:37 +0100

    webpacked

---
+++ public/components/Greeter.jsx
@@ -0,0 +1,34 @@
+var React = require('react');
+var GreeterMessage = require('./GreeterMessage');
+var GreeterForm = require('./GreeterForm');
+
+var Greeter = React.createClass({
+    getDefaultProps: function() {
+        return {
+            name: 'React',
+            message: 'This is from the default props'
+        }
+    },
+    getInitialState: function() {
+        return {
+            name: this.props.name,
+            message: this.props.message
+        };
+    },
+    handleUpdates: function(updates) {
+        this.setState(updates);
+    },
+    render: function() {
+        var name = this.state.name;
+        var message = this.state.message;
+
+        return (
+            <div>
+                <GreeterMessage name ={name} message={message}/>
+                <GreeterForm onUpdates={this.handleUpdates}/>
+            </div>
+        );
+    }
+});
+
+module.exports = Greeter;
