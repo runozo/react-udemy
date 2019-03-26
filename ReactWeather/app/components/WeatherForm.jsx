adf7546250fe60487aebace198ec874ed8021c50
Cesare Ghirelli <cghirelli@arpae.it>
2019-03-26 14:00:43 +0100

    added loading message

--- ReactWeather/app/components/WeatherForm.jsx
+++ ReactWeather/app/components/WeatherForm.jsx
@@ -1,12 +1,22 @@
 var React = require('react');
 
 var WeatherForm = React.createClass({
+    onFormSubmit: function(e) {
+        e.preventDefault();
+
+        var location = this.refs.location.value;
+
+        if (location.length > 0) {
+            this.refs.location.value = '';
+            this.props.onSearch(location);
+        }
+    },
     render: function() {
         return (
-            <form>
+            <form onSubmit={this.onFormSubmit}>
                 <h2>Get Weather</h2>
                 <div>
-                    <input type="text" ref="weathermessage"/>
+                    <input type="text" ref="location"/>
                 </div>
                 <div>
                     <button>Get Weather</button>
