7680261646559aa06a56a550891208885b669b34
Cesare Ghirelli <cghirelli@arpae.it>
2019-03-28 16:06:02 +0100

    added some search examples

--- ReactWeather/app/components/Examples.jsx
+++ ReactWeather/app/components/Examples.jsx
@@ -1,4 +1,5 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 // var Examples = React.createClass({
 //     render: function() {
@@ -8,6 +9,18 @@ var React = require('react');
 //     }
 // });
 
-var Examples = () => <h3>Examples</h3>;
+var Examples = () =>
+    <div>
+        <h1 className="text-center">Examples</h1>
+        <p>Here are a few example locations to try out:</p>
+        <ol>
+            <li>
+                <Link to='/?location=Philadelphia'>Philadelphia, PA</Link>
+            </li>
+            <li>
+                <Link to='/?location=Rio'>Rio, Brazil</Link>
+            </li>
+        </ol>
+    </div>
 
 module.exports = Examples;
